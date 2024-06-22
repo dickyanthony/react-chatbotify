33b2a7435306679c61296112b80fc5c558888fb8
dicky <dicky.a@weefer.co.id>
2024-06-22 20:26:57 +0700

    DA-feat: fork and modify custom react-chatbotify

--- src/services/BotOptionsService.tsx
+++ src/services/BotOptionsService.tsx
@@ -1,236 +1,262 @@
-import { Options } from "../types/Options";
+import { Options } from '../types/Options';
 
-import chatButton from "../assets/chat_button.png";
-import actionDisabledIcon from "../assets/action_disabled_icon.png";
-import botAvatar from "../assets/bot_avatar.png";
-import userAvatar from "../assets/user_avatar.svg";
-import chatIcon from "../assets/chat_icon.svg";
-import fileAttachmentIcon from "../assets/file_attachment_icon.svg";
-import notificationIcon from "../assets/notification_icon.svg";
-import closeChatIcon from "../assets/close_chat_icon.svg";
-import sendButtonIcon from "../assets/send_icon.svg";
-import voiceIcon from "../assets/voice_icon.svg";
-import emojiIcon from "../assets/emoji_icon.svg";
-import audioIcon from "../assets/audio_icon.svg";
-import notificationSound from "../assets/notification_sound.wav";
+import chatButton from '../assets/chat_button.png';
+import actionDisabledIcon from '../assets/action_disabled_icon.png';
+import botAvatar from '../assets/bot_avatar.png';
+import userAvatar from '../assets/user_avatar.svg';
+import chatIcon from '../assets/chat_icon.svg';
+import fileAttachmentIcon from '../assets/file_attachment_icon.svg';
+import notificationIcon from '../assets/notification_icon.svg';
+import closeChatIcon from '../assets/close_chat_icon.svg';
+import sendButtonIcon from '../assets/send_icon.svg';
+import voiceIcon from '../assets/voice_icon.svg';
+import emojiIcon from '../assets/emoji_icon.svg';
+import audioIcon from '../assets/audio_icon.svg';
+import notificationSound from '../assets/notification_sound.wav';
 
 // default options provided to the bot
 const defaultOptions = {
-	// tracks state of chat window, also the default state to load it in
-	isOpen: false,
+  // tracks state of chat window, also the default state to load it in
+  isOpen: false,
 
-	// configurations
-	theme: {
-		primaryColor: "#42b0c5",
-		secondaryColor: "#491d8d",
-		fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', " +
-			"'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', " +
-			"sans-serif",
-		showHeader: true,
-		showFooter: true,
-		showInputRow: true,
-		actionDisabledIcon: actionDisabledIcon,
-		embedded: false,
-		desktopEnabled: true,
-		mobileEnabled: true,
-		flowStartTrigger: "ON_LOAD",
-	},
-	tooltip: {
-		mode: "CLOSE",
-		text: "Talk to me! 😊",
-	},
-	chatButton: {
-		icon: chatIcon,
-	},
-	header: {
-		title: (
-			<div style={{cursor: "pointer", margin: 0, fontSize: 20, fontWeight: "bold"}} onClick={
-				() => window.open("https://github.com/tjtanjin/")
-			}>
-				Tan Jin
-			</div>
-		),
-		showAvatar: true,
-		avatar: botAvatar,
-		closeChatIcon: closeChatIcon,
-	},
-	notification: {
-		disabled: false,
-		defaultToggledOn: true,
-		volume: 0.2,
-		icon: notificationIcon,
-		sound: notificationSound,
-		showCount: true,
-	},
-	audio: {
-		disabled: true,
-		defaultToggledOn: false,
-		language: "en-US",
-		voiceNames: ["Microsoft David - English (United States)", "Alex (English - United States)"],
-		rate: 1,
-		volume: 1,
-		icon: audioIcon,
-	},
-	chatHistory: {
-		disabled: false,
-		maxEntries: 30,
-		storageKey: "rcb-history",
-		viewChatHistoryButtonText: "Load Chat History ⟳",
-		chatHistoryLineBreakText: "----- Previous Chat History -----",
-		autoLoad: false,
-	},
-	chatInput: {
-		disabled: false,
-		allowNewline: false,
-		enabledPlaceholderText: "Type your message...",
-		disabledPlaceholderText: "",
-		showCharacterCount: false,
-		characterLimit: -1,
-		botDelay: 1000,
-		sendButtonIcon: sendButtonIcon,
-		blockSpam: true,
-		sendOptionOutput: true,
-		sendCheckboxOutput: true,
-		sendAttachmentOutput: true,
-	},
-	chatWindow: {
-		showScrollbar: false,
-		autoJumpToBottom: false,
-		showMessagePrompt: true,
-		messagePromptText: "New Messages ↓",
-		messagePromptOffset: 30,
-	},
-	sensitiveInput: {
-		maskInTextArea: true,
-		maskInUserBubble: true,
-		asterisksCount: 10,
-		hideInUserBubble: false,
-	},
-	userBubble: {
-		animate: true,
-		showAvatar: false,
-		avatar: userAvatar,
-		simStream: false,
-		streamSpeed: 30,
-		dangerouslySetInnerHtml: false,
-	},
-	botBubble: {
-		animate: true,
-		showAvatar: false,
-		avatar: botAvatar,
-		simStream: false,
-		streamSpeed: 30,
-		dangerouslySetInnerHtml: false,
-	},
-	voice: {
-		disabled: true,
-		defaultToggledOn: false,
-		language: "en-US",
-		timeoutPeriod: 10000,
-		autoSendDisabled: false,
-		autoSendPeriod: 1000,
-		icon: voiceIcon,
-	},
-	footer: {
-		text: (
-			<div style={{cursor: "pointer", display: "flex", flexDirection: "row", alignItems: "center", columnGap: 3}} 
-				onClick={() => window.open("https://react-chatbotify.tjtanjin.com")}
-			>
-				<span>Powered By </span>
-				<img style={{width: 10, height: 10}} src={chatButton}/>
-				<span style={{fontWeight: "bold"}}> React ChatBotify</span>
-			</div>
-		),
-	},
-	fileAttachment: {
-		disabled: false,
-		multiple: true,
-		accept: ".png",
-		icon: fileAttachmentIcon,
-	},
-	emoji: {
-		disabled: false,
-		icon: emojiIcon,
-		list: ["😀", "😃", "😄", "😅", "😊", "😌", "😇", "🙃", "🤣", "😍", "🥰", "🥳", "🎉", "🎈", "🚀", "⭐️"]
-	},
-	advance: {
-		useCustomMessages: false,
-		useCustomBotOptions: false,
-		useCustomPaths: false,
-	},
+  // configurations
+  theme: {
+    primaryColor: '#42b0c5',
+    secondaryColor: '#491d8d',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', " +
+      "'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', " +
+      'sans-serif',
+    showHeader: true,
+    showFooter: true,
+    showInputRow: true,
+    actionDisabledIcon: actionDisabledIcon,
+    embedded: false,
+    desktopEnabled: true,
+    mobileEnabled: true,
+    flowStartTrigger: 'ON_LOAD',
+  },
+  tooltip: {
+    mode: 'CLOSE',
+    text: 'Talk to me! 😊',
+  },
+  chatButton: {
+    icon: chatIcon,
+  },
+  header: {
+    title: (
+      <div
+        style={{ cursor: 'pointer', margin: 0, fontSize: 20, fontWeight: 'bold' }}
+        onClick={() => window.open('https://github.com/dickyanthony/react-chatbotify/')}
+      >
+        Nusantara Bot
+      </div>
+    ),
+    showAvatar: true,
+    avatar: botAvatar,
+    closeChatIcon: closeChatIcon,
+  },
+  notification: {
+    disabled: false,
+    defaultToggledOn: true,
+    volume: 0.2,
+    icon: notificationIcon,
+    sound: notificationSound,
+    showCount: true,
+  },
+  audio: {
+    disabled: true,
+    defaultToggledOn: false,
+    language: 'en-US',
+    voiceNames: ['Microsoft David - English (United States)', 'Alex (English - United States)'],
+    rate: 1,
+    volume: 1,
+    icon: audioIcon,
+  },
+  chatHistory: {
+    disabled: false,
+    maxEntries: 30,
+    storageKey: 'rcb-history',
+    viewChatHistoryButtonText: 'Load Chat History ⟳',
+    chatHistoryLineBreakText: '----- Previous Chat History -----',
+    autoLoad: false,
+  },
+  chatInput: {
+    disabled: false,
+    allowNewline: false,
+    enabledPlaceholderText: 'Type your message...',
+    disabledPlaceholderText: '',
+    showCharacterCount: false,
+    characterLimit: -1,
+    botDelay: 1000,
+    sendButtonIcon: sendButtonIcon,
+    blockSpam: true,
+    sendOptionOutput: true,
+    sendCheckboxOutput: true,
+    sendAttachmentOutput: true,
+  },
+  chatWindow: {
+    showScrollbar: false,
+    autoJumpToBottom: false,
+    showMessagePrompt: true,
+    messagePromptText: 'New Messages ↓',
+    messagePromptOffset: 30,
+  },
+  sensitiveInput: {
+    maskInTextArea: true,
+    maskInUserBubble: true,
+    asterisksCount: 10,
+    hideInUserBubble: false,
+  },
+  userBubble: {
+    animate: true,
+    showAvatar: false,
+    avatar: userAvatar,
+    simStream: false,
+    streamSpeed: 30,
+    dangerouslySetInnerHtml: false,
+  },
+  botBubble: {
+    animate: true,
+    showAvatar: false,
+    avatar: botAvatar,
+    simStream: false,
+    streamSpeed: 30,
+    dangerouslySetInnerHtml: false,
+  },
+  voice: {
+    disabled: true,
+    defaultToggledOn: false,
+    language: 'en-US',
+    timeoutPeriod: 10000,
+    autoSendDisabled: false,
+    autoSendPeriod: 1000,
+    icon: voiceIcon,
+  },
+  footer: {
+    text: (
+      <div
+        style={{
+          cursor: 'pointer',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          columnGap: 3,
+        }}
+        onClick={() => window.open('https://react-chatbotify.tjtanjin.com')}
+      >
+        <span>Powered By </span>
+        <img style={{ width: 10, height: 10 }} src={chatButton} />
+        <span style={{ fontWeight: 'bold' }}> Nusantara Byte</span>
+      </div>
+    ),
+  },
+  fileAttachment: {
+    disabled: false,
+    multiple: true,
+    accept: '.png',
+    icon: fileAttachmentIcon,
+  },
+  emoji: {
+    disabled: false,
+    icon: emojiIcon,
+    list: [
+      '😀',
+      '😃',
+      '😄',
+      '😅',
+      '😊',
+      '😌',
+      '😇',
+      '🙃',
+      '🤣',
+      '😍',
+      '🥰',
+      '🥳',
+      '🎉',
+      '🎈',
+      '🚀',
+      '⭐️',
+    ],
+  },
+  advance: {
+    useCustomMessages: false,
+    useCustomBotOptions: false,
+    useCustomPaths: false,
+  },
 
-	// styles
-	tooltipStyle: {},
-	chatButtonStyle: {},
-	notificationBadgeStyle: {},
-	chatWindowStyle: {},
-	headerStyle: {},
-	bodyStyle: {},
-	chatInputContainerStyle: {},
-	chatInputAreaStyle: {},
-	chatInputAreaFocusedStyle: {},
-	chatInputAreaDisabledStyle: {},
-	userBubbleStyle: {},
-	botBubbleStyle: {},
-	botOptionStyle: {},
-	botOptionHoveredStyle: {},
-	botCheckboxRowStyle: {},
-	botCheckboxNextStyle: {},
-	botCheckMarkStyle: {},
-	botCheckMarkSelectedStyle: {},
-	sendButtonStyle: {},
-	sendButtonHoveredStyle: {},
-	characterLimitStyle: {},
-	characterLimitReachedStyle: {},
-	chatHistoryButtonStyle: {},
-	chatHistoryButtonHoveredStyle: {},
-	chatHistoryLineBreakStyle: {},
-	chatMessagePromptStyle: {},
-	chatMessagePromptHoveredStyle: {},
-	footerStyle: {},
-	loadingSpinnerStyle: {},
-}
+  // styles
+  tooltipStyle: {},
+  chatButtonStyle: {},
+  notificationBadgeStyle: {},
+  chatWindowStyle: {},
+  headerStyle: {},
+  bodyStyle: {},
+  chatInputContainerStyle: {},
+  chatInputAreaStyle: {},
+  chatInputAreaFocusedStyle: {},
+  chatInputAreaDisabledStyle: {},
+  userBubbleStyle: {},
+  botBubbleStyle: {},
+  botOptionStyle: {},
+  botOptionHoveredStyle: {},
+  botCheckboxRowStyle: {},
+  botCheckboxNextStyle: {},
+  botCheckMarkStyle: {},
+  botCheckMarkSelectedStyle: {},
+  sendButtonStyle: {},
+  sendButtonHoveredStyle: {},
+  characterLimitStyle: {},
+  characterLimitReachedStyle: {},
+  chatHistoryButtonStyle: {},
+  chatHistoryButtonHoveredStyle: {},
+  chatHistoryLineBreakStyle: {},
+  chatMessagePromptStyle: {},
+  chatMessagePromptHoveredStyle: {},
+  footerStyle: {},
+  loadingSpinnerStyle: {},
+};
 
 /**
  * Retrieves default values for bot options.
  */
 export const getDefaultBotOptions = () => {
-	return defaultOptions;
-}
+  return defaultOptions;
+};
 
 /**
  * Parses default options with user provided options to generate final bot options.
- * 
+ *
  * @param providedOptions options provided by the user to the bot
  */
 export const parseBotOptions = (providedOptions: Options | undefined) => {
-	if (!providedOptions) {
-		return defaultOptions;
-	}
+  if (!providedOptions) {
+    return defaultOptions;
+  }
 
-	// enforces value for bot delay does not go below 500
-	if (providedOptions.chatInput?.botDelay != null && providedOptions.chatInput?.botDelay < 500) {
-		providedOptions.chatInput.botDelay = 500;
-	}
+  // enforces value for bot delay does not go below 500
+  if (providedOptions.chatInput?.botDelay != null && providedOptions.chatInput?.botDelay < 500) {
+    providedOptions.chatInput.botDelay = 500;
+  }
 
-	return getCombinedOptions(providedOptions);
-}
+  return getCombinedOptions(providedOptions);
+};
 
 /**
  * Combines default and provided options.
- * 
+ *
  * @param providedOptions options provided by the user to the bot
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getCombinedOptions = (providedOptions: any): Options => {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const mergedOptions: any = { ...defaultOptions };
-	for (const prop in providedOptions) {
-		if (typeof providedOptions[prop] === "object" && providedOptions[prop] !== null) {
-			mergedOptions[prop] = { ...mergedOptions[prop], ...providedOptions[prop] };
-		} else {
-			mergedOptions[prop] = providedOptions[prop];
-		}
-	}
-	return mergedOptions;
-}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const mergedOptions: any = { ...defaultOptions };
+  for (const prop in providedOptions) {
+    if (typeof providedOptions[prop] === 'object' && providedOptions[prop] !== null) {
+      mergedOptions[prop] = { ...mergedOptions[prop], ...providedOptions[prop] };
+    } else {
+      mergedOptions[prop] = providedOptions[prop];
+    }
+  }
+  return mergedOptions;
+};
